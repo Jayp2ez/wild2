f7279746b9fb9c94cd3a964ad66aa2a7f504a89b
Jayp2ez <104533841+Jayp2ez@users.noreply.github.com>
2025-11-30 22:02:32 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,9 +6,10 @@ window._config = {
         region: 'us-west-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://xxxxxx.execute-api.us-west-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
